5eb6a79c24b2cfb75982037419381e07fa445612
Slusks <samslusky@gmail.com>
2020-02-24 11:19:00 -0500

    2-24-2020 Morning
    - generating the markers and form works.
    - very little about reloading the page works.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, ViewChild, ElementRef, OnInit, AfterViewInit, Input } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { DndDatabaseService } from './dnd-database.service';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule }   from '@angular/forms';
+import { isNgTemplate } from '@angular/compiler';
+import { markerData } from './markerData';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +13,63 @@ import { HttpClient } from '@angular/common/http'
 export class AppComponent implements OnInit {
   title = 'map-app';
   id: number;
+  jsonmarkerData;
 
-  constructor(private http: HttpClient){}
+  constructor(private dndDatabaseService: DndDatabaseService,
+              private http: HttpClient){};
+  
 
   iconDescription:object={};
-  confirmationString:string = "Marker Added!";
-  isAdded: boolean = false;
+
 
  
   ngOnInit(){
     const iconImage = new Image();
     iconImage.src = "../assets/img/marker.jpg"
     var map = document.getElementById('map')
-    map.addEventListener("click", this.logCursorPosition)    
-  }
+    map.addEventListener("click", this.logCursorPosition)
+
+    //subscribes to the JSON file
+    this.dndDatabaseService.getmapMarkers().subscribe(posts => {posts.forEach(post =>{setMarker(post)})}) // console log so we know what we're getting
+
+    /*this.http.get('http://localhost:3000/mapMarker')
+        .subscribe(posts => {
+            posts.forEach(post=>{
+                  setMarker(post)})})*/
+
+    //this.markerData = this.dndDatabaseService.getmapMarkers(); // puts the data into a usable variable
+
+
+    
+    
+    //function to programmatically generate the markers from the json data
+
+    
+    function setMarker(post){
+      console.log("function started", post.id)
+      var description = post.description;
+      var x = post.xPos;
+      var y = post.yPos;
+      var img = document.createElement("img");
+      img.src = "../assets/img/marker.jpg";
+      img.width = 20;
+      img.height= 20;
+      img.style.position="absolute";
+      img.style.left= (x-20)+'px';
+      img.style.top=(y-40)+'px';
+      document.getElementById('map container').appendChild(img);
+      console.log("function ran")
+    }
+
+
+    }
+
+getCursorPosition(e){
+  var x = (e.clientX)-20;
+  var y = (e.clientY)-40;
+  console.log(x , y)
+  return {"x": x, "y": y}
+}
 
 
 //this function now places the icon but I dont want to change the name yet
@@ -32,67 +79,56 @@ logCursorPosition(e){
   var y = e.clientY;
   console.log("X Position "+ x + " Y Position" +y);
   //place Icon
-  var img = document.createElement("img");
-  img.src = "../assets/img/marker.jpg";
-  img.width = 20;
-  img.height= 20;
-  img.style.position="absolute";
-  img.style.left= (x-20)+'px';
-  img.style.top=(y-40)+'px';
-  document.getElementById('map container').appendChild(img);
+    var img = document.createElement("img");
+    img.src = "../assets/img/marker.jpg";
+    img.width = 20;
+    img.height= 20;
+    img.style.position="absolute";
+    img.style.left= (x-20)+'px';
+    img.style.top=(y-40)+'px';
+    document.getElementById('map container').appendChild(img);
+    img.id="currentMarker"
+
 
   // I am not sure why this doesn't work in the openForm function below
   //leads to TypeError: this.openForm() is not a function
-  //Otherwise, the below opens the form and puts it where it needs to be
-  document.getElementById("myForm").style.display = "block";
-  document.getElementById('myForm').style.top = y +'px';
-  document.getElementById('myForm').style.left = x +'px';
+  
+  document.getElementById("myForm").style.display = "block"; //Opens the form
+  document.getElementById('myForm').style.top = y +'px'; // sets the form y coordinate
+  document.getElementById('myForm').style.left = x +'px'; // sets the form x coordinate
+  
+  const coordinate = {"x":x-20, "y":y-40}
+  return coordinate
   }
 
-
-// functions for increasing the id number because this is something that i will need to do.
-step = 0;
-setStep(index:number){
-  this.step =index;
-}
-nextStep() {
-  this.step++;
-}
-prevStep() {
-  this.step--;
-}
-
 //function that opens and closes the form for the icons, currently doesn't work
-openForm(){
-  document.getElementById("myForm").style.display = "block";
-}
-
 closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
+/* Submit form needs to do the following:
+ - Add Description, xpos, and ypos to json file
+ - change img.id to a number */
 
-//Actual hard part for subscribing to the json file
-updateMarkerJSON(mapMarker){
-  this.iconDescription ={
-    "description": mapMarker.description,
-    "xPos": mapMarker.xPos,
-    "yPos": mapMarker.yPos,
+
+
+
+ markerFormSubmit(marker, position){
+    console.log("coordinate", this.logCursorPosition)
+    this.iconDescription ={
+      "description": marker.description,
+      "xPos":position.x,
+      "yPos":position.y,
   }
-  
-  this.http.post("http://localhost:3000/mapMarker", this.iconDescription).subscribe((po:Response) => {console.log("po",po)})
-  this.isAdded = true;
-  location.reload(true)
-}
+    this.http.post("http://localhost:3000/mapMarker", this.iconDescription).subscribe((po:Response) => {console.log("po",po)})
+    
+    document.getElementById("myForm").style.display = "none";
+    alert("Description entered")
+   }
 
 
 
 
+ }
 
 
-
-
-
-
-
-}
